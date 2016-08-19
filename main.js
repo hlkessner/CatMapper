844d75742bc6df47ddaa65654996135a962f789d
kessnerh <kessnerh@seattlu.edu>
2016-08-19 14:43:02 -0400

    Moved API

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ $('#myTabs a').click(function (e) {
 });
 
 //map container
+
 var mymap = L.map('mapid').setView([47.606662, -122.326987], 12);
 L.tileLayer('https://api.mapbox.com/styles/v1/hlkessner/cirr7avti0009gbm2595xkzwi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGxrZXNzbmVyIiwiYSI6ImNpcXU3ZWh5MTAwYWVmcm5odmZjaGNzY3UifQ.EIAGUVi9S2Ew6ajp2sBT8Q', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -32,7 +33,66 @@ L.tileLayer('https://api.mapbox.com/styles/v1/hlkessner/cirr7avti0009gbm2595xkzw
 
 // mymap.on('locationerror', onLocationError);
 
+ var url = 'http://api.petfinder.com/pet.getRandom?key=12ca8a73b3b4a51797988576ecfd5ebc&shelterid=WA119&output=full&format=json';
+    $.ajax({
+        type : 'GET',
+        data : {},
+        url : url+'&callback=?' ,
+        dataType: 'json',
+        success : function(data) {              
+            // stores result
+            var result = '';
 
+            var petfinder = data.petfinder;
+            var infoHTML = '<ul>';
+            infoHTML += '<li>';
+            infoHTML += '<strong>Description</strong><br>';
+            infoHTML += petfinder.pet.description.$t;
+            infoHTML += '</li>';
+
+            infoHTML += '<li>';
+//             infoHTML += '<strong>Breed</strong><br>';
+//             $.each(petfinder.pet.breeds.breed, function(i, breed){
+//                 infoHTML += '<p>'+ breed['$t'] + '</p>';
+//             });
+            infoHTML += '</li>';
+
+            infoHTML += '<li>';
+            infoHTML += '<strong>shelterId</strong><br>';
+            infoHTML += petfinder.pet.shelterId['$t'];
+            infoHTML += '</li>';
+            infoHTML += '<li>';
+            infoHTML += '<strong>Images WITH SIZE</strong><br>';
+            $.each(petfinder.pet.media.photos.photo, function(i, image){
+                if(image['@size'] == 'x' && image['@id'] == '1'){
+                    infoHTML += '<img src="'+image['$t']+'" />';
+                }
+
+            });
+            infoHTML += '</li>';
+
+            infoHTML += '<li>';
+            infoHTML += '<strong>Images</strong><br>';
+            $.each(petfinder.pet.media.photos.photo, function(i, image){
+                infoHTML += '<img src="'+image['$t']+'" />';
+
+            });
+            infoHTML += '</li>';
+
+            infoHTML += '</ul>';
+            // return infoHTML;
+            $('#petfinderInfo').html(infoHTML);
+
+            // $('#petfinderInfo').html(petfinder.pet.description['$t']);
+            // 
+            console.log(petfinder);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(request));
+        }
+    });
+            
 
 
 //shelters
