a86abeb0c12ae2f19b90f68fd1a917965a8375e7
kessnerh <kessnerh@seattlu.edu>
2016-08-21 18:34:11 -0400

    Completed final project checkin

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/hlkessner/cirr7avti0009gbm2595xkzw
 
 // mymap.on('locationerror', onLocationError);
 
- var url = 'http://api.petfinder.com/pet.getRandom?key=12ca8a73b3b4a51797988576ecfd5ebc&shelterid=WA59&output=full&format=json';
+ var url = 'http://api.petfinder.com/pet.getRandom?key=12ca8a73b3b4a51797988576ecfd5ebc&shelterid=WA40&output=full&format=json';
     $.ajax({
         type : 'GET',
         data : {},
@@ -99,7 +99,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/hlkessner/cirr7avti0009gbm2595xkzw
 //Kitty Harbor
 
 var harborMarker = L.marker ([47.572180, -122.370493]).addTo(mymap);
-harborMarker.bindPopup("<b>Kitty Harbor</b><br>3422 Harbor Ave. SW<br>Seattle, WA 98126<br>Hours: Saturday & Sunday 12:00-4:00<br>http://www.kittyharborseattle.org/").openPopup();
+harborMarker.bindPopup("<b>Kitty Harbor</b><br>3422 Harbor Ave. SW<br>Seattle, WA 98126<br>Saturday & Sunday 12:00-4:00<br>http://www.kittyharborseattle.org/").openPopup();
 //Seattle Animal Shelter
 var shelterMarker = L.marker ([47.638236, -122.376523]).addTo(mymap);
 shelterMarker.bindPopup("<b>Seattle Animal Shelter</b><br>2061 15th Ave W<br>Seattle, WA 98119<br> Tuesday-Sunday 12:00-6:00<br>http://www.seattle.gov/animalshelter>").openPopup();
@@ -108,14 +108,14 @@ var pawsMarker = L.marker ([47.666603, -122.317106]).addTo(mymap);
 pawsMarker.bindPopup("<b>P.A.W.S Cat City</b><br>5200 Roosevelt Way NE, Suite B<br>Seattle, WA 98105<br>Monday-Friday 11:00-6:00<br>Saturday & Sunday 11:00-5:00<br>Closed on Tuesdays<br>https://www.paws.org/cats-and-dogs/shelter/").openPopup();
   //Emerald City Pet Rescue
 var emeraldMarker = L.marker ([47.575794, -122.333167]).addTo(mymap);
-emeraldMarker.bindPopup("<b>Emerald City Pet Rescue</b>").openPopup();
+emeraldMarker.bindPopup("<b>Emerald City Pet Rescue</b><br>2962 First Avenue South, Suite B<br>Seattle, WA. 98134<br>Monday-Friday 10:00am- 7:00pm<br>Saturday 10:00am-6:00pm<br>Sunday 12:00pm-5:00pm<br>http://www.emeraldcitypetrescue.org/<br>").openPopup();
 //Seattle Area Feline Rescue
 var felineMarker = L.marker ([47.735476, -122.345813]).addTo(mymap);
-felineMarker.bindPopup("<b>Seattle Area Feline Rescue</b>").openPopup();
+felineMarker.bindPopup("<b>Seattle Area Feline Rescue</b><br>14717 Aurora Ave. North<br>Shoreline, WA 98133<br>Friday-Tuesday 12:00pm-7:00pm<br>http://www.seattleareafelinerescue.org/").openPopup();
 //Meow Cat Rescue
 var meowMarker = L.marker ([47.667306, -122.198313]).addTo(mymap);
-meowMarker.bindPopup("<b>Meow Cat Rescue</b>").openPopup();
+meowMarker.bindPopup("<b>Meow Cat Rescue</b><br>10600 NE 68th, Suite F<br>Kirkland, WA 98033<br>Tuesday & Wednesday 12:00pm-5:00pm<br>Thursday 3:00pm-7:00pm<br>Saturday 11:00am-5:00pm<br>Sunday 12:00pm-5:00pm<br>http://meowcatrescue.org/<br>").openPopup();
 //Seattle Humaine
-var humaineMarker = L.marker ([47.580950, -122.163706]).addTo(mymap);
-humaineMarker.bindPopup("<b>Seattle Humaine</b>").openPopup();
+var humaneMarker = L.marker ([47.580950, -122.163706]).addTo(mymap);
+humaneMarker.bindPopup("<b>Seattle Humane</b><br>13212 SE Eastgate Way<br>Bellevue, WA 98005<br>Sunday-Wednesday 11:00am-6:00pm<br>Thursday-Saturday 11:00am-7:00pm<br>http://www.seattlehumane.org/<br>").openPopup();
 
