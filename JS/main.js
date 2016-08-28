a599e81cad204c25df930f7d0bab79b138804b8b
kessnerh <kessnerh@seattlu.edu>
2016-08-28 13:54:05 -0400

    Completed CatMapper final project

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
   //on click event
-
+$('.btn-login').on('click',function(){
+  var email=$('.email-input').val();
+  //hides the login form when the user logs into the page
+  $("#login-form").hide();
+  //fills in the users info once they're logged in 
+  alert("Welcome! You are sucessfully loged in!");
+});
 
   //bootstrap commands for tabs
 $('#myTabs a').click(function (e) {
@@ -86,13 +92,11 @@ L.tileLayer('https://api.mapbox.com/styles/v1/hlkessner/cirr7avti0009gbm2595xkzw
     });
             
 var catIcon = L.icon({
-    iconUrl: 'img/cat mapper icon.png',
-    //shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
+    iconUrl: 'img/cat_mapper_icon.png',
+    iconSize:     [50, 50], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [25, 50], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
@@ -102,22 +106,22 @@ var catIcon = L.icon({
 var harborMarker = L.marker ([47.572180, -122.370493],{icon: catIcon}).addTo(mymap);
 harborMarker.bindPopup("<b>Kitty Harbor</b><br>3422 Harbor Ave. SW<br>Seattle, WA 98126<br>Saturday & Sunday 12:00-4:00<br><a href=http://www.kittyharborseattle.org/>Website</a>").openPopup();
 //Seattle Animal Shelter
-var shelterMarker = L.marker ([47.638236, -122.376523]).addTo(mymap);
+var shelterMarker = L.marker ([47.638236, -122.376523],{icon: catIcon}).addTo(mymap);
 shelterMarker.bindPopup("<b>Seattle Animal Shelter</b><br>2061 15th Ave W<br>Seattle, WA 98119<br> Tuesday-Sunday 12:00-6:00<br><a href=http://www.seattle.gov/animalshelter>Website</a>").openPopup();
 //PAWS Cat City
-var pawsMarker = L.marker ([47.666603, -122.317106]).addTo(mymap);
+var pawsMarker = L.marker ([47.666603, -122.317106],{icon: catIcon}).addTo(mymap);
 pawsMarker.bindPopup("<b>P.A.W.S Cat City</b><br>5200 Roosevelt Way NE, Suite B<br>Seattle, WA 98105<br>Monday-Friday 11:00-6:00<br>Saturday & Sunday 11:00-5:00<br>Closed on Tuesdays<br><a href=https://www.paws.org/cats-and-dogs/shelter/>Website</a>").openPopup();
   //Emerald City Pet Rescue
-var emeraldMarker = L.marker ([47.575794, -122.333167]).addTo(mymap);
+var emeraldMarker = L.marker ([47.575794, -122.333167],{icon: catIcon}).addTo(mymap);
 emeraldMarker.bindPopup("<b>Emerald City Pet Rescue</b><br>2962 First Avenue South, Suite B<br>Seattle, WA. 98134<br>Monday-Friday 10:00am- 7:00pm<br>Saturday 10:00am-6:00pm<br>Sunday 12:00pm-5:00pm<br><a href=http://www.emeraldcitypetrescue.org/>Website</a><br>").openPopup();
 //Seattle Area Feline Rescue
-var felineMarker = L.marker ([47.735476, -122.345813]).addTo(mymap);
+var felineMarker = L.marker ([47.735476, -122.345813],{icon: catIcon}).addTo(mymap);
 felineMarker.bindPopup("<b>Seattle Area Feline Rescue</b><br>14717 Aurora Ave. North<br>Shoreline, WA 98133<br>Friday-Tuesday 12:00pm-7:00pm<br><a href=http://www.seattleareafelinerescue.org/>Website</a>").openPopup();
 //Meow Cat Rescue
-var meowMarker = L.marker ([47.667306, -122.198313]).addTo(mymap);
+var meowMarker = L.marker ([47.667306, -122.198313],{icon: catIcon}).addTo(mymap);
 meowMarker.bindPopup("<b>Meow Cat Rescue</b><br>10600 NE 68th, Suite F<br>Kirkland, WA 98033<br>Tuesday & Wednesday 12:00pm-5:00pm<br>Thursday 3:00pm-7:00pm<br>Saturday 11:00am-5:00pm<br>Sunday 12:00pm-5:00pm<br><a href=http://meowcatrescue.org/>Website</a><br>").openPopup();
 //Seattle Humaine
-var humaneMarker = L.marker ([47.580950, -122.163706]).addTo(mymap);
+var humaneMarker = L.marker ([47.580950, -122.163706],{icon: catIcon}).addTo(mymap);
 humaneMarker.bindPopup("<b>Seattle Humane</b><br>13212 SE Eastgate Way<br>Bellevue, WA 98005<br>Sunday-Wednesday 11:00am-6:00pm<br>Thursday-Saturday 11:00am-7:00pm<br><a href=http://www.seattlehumane.org/>Website</a><br>").openPopup();
 
 
